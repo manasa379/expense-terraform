bf44b44616f6667566b825745ea58998df308e87
DELL <manasajalagam379@gmail.com>
2024-04-05 15:47:47 +0530

    expense project

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "./modules/vpc"
   vpc_cidr = var.vpc_cidr
   env = var.env
-  private_subnets = var.private_subnets
   public_subnets =  var.public_subnets
+  private_subnets = var.private_subnets
   azs =  var.azs
 }
