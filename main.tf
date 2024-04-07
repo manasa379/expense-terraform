d1d9fda3e4882de59ed638e264dd9ced29788dda
DELL <manasajalagam379@gmail.com>
2024-04-07 14:44:53 +0530

    creating route tables

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_cidr = var.vpc_cidr
-  env = var.env
-  public_subnets =  var.public_subnets
-  private_subnets = var.private_subnets
-  azs =  var.azs
-  peer_owner_id = var.account_no
-  peer_vpc_id   = var.default_vpc_id
+  source                = "./modules/vpc"
+  vpc_cidr              = var.vpc_cidr
+  env                   = var.env
+  public_subnets        = var.public_subnets
+  private_subnets       = var.private_subnets
+  azs                   = var.azs
+  peer_owner_id         = var.account_no
+  peer_vpc_id           = var.default_vpc_id
 }
