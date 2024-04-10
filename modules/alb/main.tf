e896435e9b66b91163ca3956bc8735bd9aee0f39
DELL <manasajalagam379@gmail.com>
2024-04-10 20:44:28 +0530

    creating public load balancer

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "security_group" {
   }
 }
 
-resource "aws_vpc_security_group_ingress" "ingress" {
+ ingress {
   security_group_id = aws_security_group.security_group.id
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
 }
 
-resource "aws_vpc_security_group_egress" "egress" {
+egress {
   security_group_id = aws_security_group.security_group.id
   cidr_blocks       = ["0.0.0.0/0"]
   ip_protocol       = "-1"
