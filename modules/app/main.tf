3c7932cc7aa2c1c4d7b478e5bca0790e823bec4a
DELL <manasajalagam379@gmail.com>
2024-04-11 21:15:48 +0530

    Launching a template

---
+++ modules/app/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "security_group" {
+name        = "${var.env}-${var.component}-sg"
+description = "${var.env}-${var.component}-sg"
+vpc_id      = var.vpc_id
+
+ingress {
+  description = "HTTP"
+  from_port   = var.app_port
+  to_port     = var.app_port
+  protocol    = "tcp"
+  cidr_blocks = [var.vpc_cidr]
+}
+
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+tags = {
+  Name = "${var.env}-${var.component}-sg"
+}
+}
+resource "aws_launch_template" "template" {
+  name          = "${var.env}-${var.component}"
+  image_id      = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = aws_security_group.security_group.id
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${var.env}-${var.component}"
+    }
+  }
+}
---
+++ modules/app/vars.tf
@@ -0,0 +1,6 @@
+variable "env" {}
+variable "component" {}
+variable "instance_type" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
+variable "app_port" {}
