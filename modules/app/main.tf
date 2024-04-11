1216c6c974041f6819ae00292338b4d54eecd285
DELL <manasajalagam379@gmail.com>
2024-04-11 21:31:57 +0530

    Launching a template

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -36,3 +36,14 @@ resource "aws_launch_template" "template" {
     }
   }
 }
+resource "aws_autoscaling_group" "asg" {
+  name               = "${var.env}-${var.component}"
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+}
