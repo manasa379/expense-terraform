8fdedb66da1316466ca947c63b56bfdd9cef4e4c
DELL <manasajalagam379@gmail.com>
2024-04-11 15:18:21 +0530

    creating public load balancer

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -2,23 +2,23 @@ resource "aws_security_group" "security_group" {
   name        = "${var.env}-${var.alb_type}-sg"
   description = "${var.env}-${var.alb_type}-sg"
   vpc_id      = var.vpc_id
-}
-resource "aws_vpc_security_group_ingress_rule" "security_group"  {
-  security_group_id = aws_security_group.security_group.id
-  cidr_blocks       =  [var.alb_sg_allow_cidr]
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-resource "aws_vpc_security_group_egress_rule" "security_group" {
-  security_group_id = aws_security_group.security_group.id
-  cidr_blocks       = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
-tags = {
-  Name = "${var.env}-${var.alb_type}-sg"
-}
 
+  resource "aws_vpc_security_group_ingress_rule" "security_group" {
+    security_group_id = aws_security_group.security_group.id
+    cidr_blocks       = [var.alb_sg_allow_cidr]
+    from_port         = 80
+    ip_protocol       = "tcp"
+    to_port           = 80
+  }
+  resource "aws_vpc_security_group_egress_rule" "security_group" {
+    security_group_id = aws_security_group.security_group.id
+    cidr_blocks       = "0.0.0.0/0"
+    ip_protocol       = "-1"
+  }
+  tags = {
+    Name = "${var.env}-${var.alb_type}-sg"
+  }
+}
 resource "aws_lb" "alb_type" {
   name               = "${var.env}-${var.alb_type}"
   internal           = var.internal
