af4bd609843b9b8a3262f3280cdea93212e2816b
DELL <manasajalagam379@gmail.com>
2024-04-12 08:44:22 +0530

    Launching a template

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -26,7 +26,7 @@ resource "aws_launch_template" "template" {
   image_id      = data.aws_ami.ami.image_id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = aws_security_group.security_group.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
 
   tag_specifications {
     resource_type = "instance"
