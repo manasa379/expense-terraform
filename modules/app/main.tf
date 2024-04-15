da594531fec939bcc2ccd496a5b7e88a5b8df5f9
DELL <manasajalagam379@gmail.com>
2024-04-15 17:08:52 +0530

    creating instance profile

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -74,6 +74,7 @@ resource "aws_launch_template" "template" {
   image_id               = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
+
   iam_instance_profile {
     name = aws_iam_instance_profile.instance_profile.name
   }
