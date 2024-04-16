c061483f261f3a0d231ad090a34482c1cea6563f
DELL <manasajalagam379@gmail.com>
2024-04-16 18:14:49 +0530

    expense

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yum install ansible python3.11-pip -y &>>/opt/userdata.log
+dnf install ansible python3.11-pip -y &>>/opt/userdata.log
 pip3.11 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/manasa379/infra-ansible.git main.yml -e role_name=${role_name}
