3804c678e9b86f7d7a65f49230f196dd6660ec96
DELL <manasajalagam379@gmail.com>
2024-04-17 12:52:05 +0530

    adding route tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -77,7 +77,7 @@ resource "aws_route_table" "private" {
 }
 resource "aws_route" "default_route_table" {
   route_table_id            = var.default_route_table_id
-  destination_cidr_block    = var.default_vpc_cidr
+  destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 resource "aws_route_table_association" "public" {
