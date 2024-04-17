899437ecbc72b7a062c0d3046aa5d667635cc896
DELL <manasajalagam379@gmail.com>
2024-04-17 08:01:37 +0530

    creating igw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,3 +22,10 @@ resource "aws_subnet" "private_subnets" {
     Name = "private_subnet_${count.index+1}"
   }
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
