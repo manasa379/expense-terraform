8637d9d90b7b82e1107c8edcb85abc8054ed97c6
DELL <manasajalagam379@gmail.com>
2024-04-18 19:16:30 +0530

    autoscaling group

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install ansible python3.11-pip -y
-pip3.11 install botocore boto3
+dnf install ansible python3.11-pip -y &>>/opt/userdata.log
+pip3.11 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/manasa379/infra-ansible.git main.yml -e role_name=${role_name}
