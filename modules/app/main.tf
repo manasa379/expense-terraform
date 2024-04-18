9978f01a8d1942ec59ac44ab567c4234016eb056
DELL <manasajalagam379@gmail.com>
2024-04-18 21:38:36 +0530

    autoscaling group

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -17,7 +17,6 @@ resource "aws_security_group" "security_group" {
     protocol    = "tcp"
     cidr_blocks = var.bastion_node_cidr
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -28,6 +27,47 @@ resource "aws_security_group" "security_group" {
       Name = "${var.env}-${var.component}-sg"
     }
   }
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+           sid    = "VisualEditor0",
+           Effect = "Allow"
+           Action = [
+             "ssm:DescribeParameters",
+             "ssm:GetParameterHistory",
+             "ssm:GetParametersByPath",
+             "ssm:GetParameters",
+             "ssm:GetParameter"
+             ],
+             Resource = "*"
+        }
+        ]
+        })
+    }
+  tags = {
+    tag-key = "${var.env}-${var.component}-role"
+  }
+}
 resource "aws_launch_template" "template" {
   name                   = "${var.env}-${var.component}"
   image_id               = data.aws_ami.ami.id
