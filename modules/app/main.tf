fa9ed292e17172b31e74c06375ff860c6a93e1fa
DELL <manasajalagam379@gmail.com>
2024-04-27 17:51:54 +0530

    adding the target group

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -101,7 +101,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = 1
   min_size            = 1
   vpc_zone_identifier = var.subnets
-  target_group_arns   = [aws_lb_target_group.tg.arn]
+  target_group_arn    = [aws_lb_target_group.tg.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
